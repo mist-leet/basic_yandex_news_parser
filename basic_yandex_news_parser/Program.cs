738ca0ed1e4168f33697bd14a169d0f3ac25f18d
mist-leet <fufik123>
2020-08-03 15:16:29 +0300

    init

---
+++ basic_yandex_news_parser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace basic_yandex_news_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
