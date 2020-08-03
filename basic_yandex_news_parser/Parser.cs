710e164e28a1426b6458eeeca404f69bfd57d15d
mist-leet <fufik123>
2020-08-03 21:26:22 +0300

    add yandex news parser

---
+++ basic_yandex_news_parser/Parser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+using System.Xaml;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace basic_yandex_news_parser
+{
+    class Parser
+    {
+
+        public Parser()
+        {
+            var url = @"https://yandex.ru/";
+
+            HtmlWeb webDoc = new HtmlWeb();
+            HtmlDocument doc = webDoc.Load(url);
+            //"//span[@class='news__item - content ']" 
+
+            List<HtmlNode> nodes = doc.DocumentNode.SelectNodes("//ol[@class='list b-news-list']").ToList();
+
+            if (nodes != null)
+            {                
+                foreach(HtmlNode node in nodes[0].ChildNodes)
+                {
+                    Console.WriteLine(node.InnerText);
+                    Console.WriteLine(new string('=', node.InnerText.Length));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong :(");
+            }
+           
+
+        }
+    }
+}
---
+++ basic_yandex_news_parser/main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_yandex_news_parser
+{
+    class NewsParser
+    {
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                Parser parser = new Parser();
+            }
+        }
+    }
+}
